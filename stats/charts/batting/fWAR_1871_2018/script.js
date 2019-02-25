d562b901220398f4a5acef7fac105f3180d3d4a4
DESKTOP-V9NAO6B\Winston <wsbraves@gmail.com>
2019-02-24 22:28:10 -0500

    update fWAR batters from 1871 to 2018

--- stats/charts/batting/fWAR_1871_2018/script.js
+++ stats/charts/batting/fWAR_1871_2018/script.js
@@ -1,6 +1,6 @@
 /////  DIMENSIONS and MARGINS  /////
-let fullWidth = 1200,
-    fullHeight = 720;
+let fullWidth = 1320,
+    fullHeight = 800;
 
 let margin = {top: 80, right: 20, bottom: 80, left: 80},
     width = fullWidth - margin.right - margin.left,
@@ -10,6 +10,7 @@ let margin = {top: 80, right: 20, bottom: 80, left: 80},
 let selectedPoint;
 let isZoomed = false;
 let lastEvent;
+let prev_seasons;
 
 /////  CHART DIV  /////
 let chart = d3.select("div#chart");
@@ -23,7 +24,7 @@ let g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 let svgBorderWidth = 5;
-svg.style("background-color", "#99CF81")
+svg.style("background-color", "#88B873")
     //.style("border", svgBorderWidth + "px solid black");
 /////  CANVAS  /////
 let canvas = d3.select("#chart-inner")
@@ -41,19 +42,19 @@ g.append("text")
     .style("text-anchor", "middle")
     .style("font-family", "Calibri")
     .style("font-weight", "bold")
-    .style("font-size", "36px");
+    .style("font-size", "40px");
 
 // Subtitle //
 let subtitle = "All position players from 1871 to 2018";
 g.append("text")
     .attr("class", "subtitle")
     .attr("x", width / 2)
-    .attr("y", -21)
+    .attr("y", -18)
     .text(subtitle)
     .style("text-anchor", "middle")
     .style("font-family", "Cambria")
     .style("font-style", "italic")
-    .style("font-size", "17px");
+    .style("font-size", "18px");
 
 /////  RESET BUTTON  /////
 let resetButton = chart.append("button")
@@ -69,6 +70,7 @@ let resetButton = chart.append("button")
     .style("color", "white")
     .style("background-color", "#4D514C")
     .style("cursor", "pointer")
+    .style("padding", "4px")
     .text("Reset View");
 
 /////  TOOLTIP  /////
@@ -106,13 +108,7 @@ d3.select("#tooltip").append("div").attr("class", "results");
 d3.select("#tooltip").classed("hidden", true);
 
 
-// /////  PLAYER SEARCH  /////
-// let searchBox = chart.append("input")
-//     .attr("list", "names")
-//     .attr("class", "search")
-//     .style("position", "absolute")
-//     .style("right", (margin.right) + "px")
-//     .style("top", (margin.top) + "px");
+
 
 /////  SCALES - ranges  /////
 let x = d3.scaleLinear().range([0, width])
@@ -122,7 +118,7 @@ let zoomedX = d3.scaleLinear().range([0, width]),
 
 
 /////  CSV  /////
-let csvfile = "fWAR_batters_1871_2018.csv?1";
+let csvfile = "fWAR_batters_1871_to_2018.csv";
 d3.csv(csvfile, function(d, i) {
   return {
     name: d.Name,
@@ -132,18 +128,21 @@ d3.csv(csvfile, function(d, i) {
     PA: +d.PA,
     id: d.playerid,
     selected: false,
+    currentPlayer: false,
     index: i,
   };
 }).then(function(data) {
   let dataset = data.filter(d => d.fwar >= 6);
+  console.log(dataset);
+  // let ids = dataset.map(d => d.id);
+  // let unique_ids = ids.filter((d, i) => ids.indexOf(d) == i);
+
   let quadTree = d3.quadtree()
       .x(d => d.year)
       .y(d => d.fwar)
       .addAll(dataset);
       
-  console.log(dataset);
-  console.log(quadTree);
-  console.log(quadTree.data());
+
 
   /////  SCALES - domains  /////
   let xDomain = d3.extent(dataset, d => d.year),
@@ -169,7 +168,7 @@ d3.csv(csvfile, function(d, i) {
       .style("text-anchor", "middle")
       .style("fill", "black")
       .style("font-weight", "bold")
-      .style("letter-spacing", "1px")
+      .style("letter-spacing", "0.5px")
       .text("Year");
 
   let yAxis = d3.axisLeft(y).tickFormat(d3.format("0.1f")).tickSize(-width);
@@ -193,15 +192,15 @@ d3.csv(csvfile, function(d, i) {
       .style("opacity", "0.7")
 
   d3.selectAll(".tick text")
-      .style("font-size", 18)
+      .style("font-size", 19)
       .style("font-family", "Calibri")
       .style("font-weight", "bold");
   
   /////  ZOOM  /////
   let r = 5, k = [1, 13], pad = 100, tk = 1;
   let rScale = d3.scaleLinear().domain(k).range([r, 2]),
-      zoomedR = 5;//d3.scaleLinear().domain([r, 2]).range([r, 2]);
-  let tScale = d3.scaleLinear().domain(k).range([18, 16]);
+      zoomedR = 5;
+  let tScale = d3.scaleLinear().domain(k).range([19, 17]);
   let zoom = d3.zoom()
       .scaleExtent(k)
       .translateExtent([[-pad, -pad/2], [width + pad, height + pad / 2]])
@@ -220,10 +219,7 @@ d3.csv(csvfile, function(d, i) {
   function onClick() {
     let mouse = d3.mouse(this);
 
-    //let newX = d3.scaleLinear().domain()
-   // let newY = d3.event.transform.rescaleY(y);
     let xPosition, yPosition;
-    console.log(isZoomed)
     if (isZoomed) {
       xPosition = zoomedX.invert(mouse[0]),
       yPosition = zoomedY.invert(mouse[1]);
@@ -231,41 +227,38 @@ d3.csv(csvfile, function(d, i) {
       xPosition = x.invert(mouse[0]),
       yPosition = y.invert(mouse[1]);
     }
-    // let xPosition = x.invert(mouse[0]),
-    //     yPosition = y.invert(mouse[1]);
-    //console.log(mouse[0], xPosition);
-    //console.log(mouse[1], yPosition);
+
     let closest = quadTree.find(xPosition, yPosition);
-    
     if (closest != undefined) {
       let dx = zoomedX(closest.year),
           dy = zoomedY(closest.fwar);
       
-      console.log(closest.year, closest.fwar, mouse[0], mouse[1], dx, dy);
       let distance = euclideanDistance(mouse[0], mouse[1], dx, dy);
-      console.log(distance, zoomedR);
       
       if (distance < zoomedR) {
         if (selectedPoint != undefined) {
           if (selectedPoint == closest) {
             if (selectedPoint.selected) {
               selectedPoint.selected = false;
+              drawPoint(selectedPoint, r);
               d3.select("#tooltip").classed("hidden", true)
             } else {
-              
               selectedPoint.selected = true;
+              drawPoint(selectedPoint, r);
               d3.select("#tooltip").classed("hidden", false)
             }
           } else {
             selectedPoint.selected = false;
-            //drawPoint(selectedPoint, r);
+            drawPoint(selectedPoint, r);
             selectedPoint = closest;
             selectedPoint.selected = true;
+            drawPoint(selectedPoint, r);
             d3.select("#tooltip").classed("hidden", false)
           }
         } else {
           selectedPoint = closest;
           selectedPoint.selected = true;
+          drawPoint(selectedPoint, r);
           d3.select("#tooltip").classed("hidden", false)
         }
         
@@ -277,23 +270,26 @@ d3.csv(csvfile, function(d, i) {
         d3.selectAll(".results").remove();
         selectedPoints = dataset.filter(d => d.year == selectedPoint.year && d.fwar == selectedPoint.fwar);
         selectedPoints.forEach((d, i) => {
+          let year_url = "https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=0&type=8&season=" + d.year + "&month=&season1=&ind=&team=&rost=&age=&filter=&players=";
           if (i == 0) {
-            d3.select("span#year")
-                .text(d.year);
+            d3.select("a#year")
+                .attr("href", year_url)
+                .attr("target", "_blank")
+                .text(d.year)
+                .style("", "underlined");
             d3.select("span#fwar")
                 .text(d3.format("0.1f")(d.fwar));
             // d3.select("#tooltip").append("div")
             //   .text("Player\tOverall Rank");
           }
-          let url = "https://www.fangraphs.com/statss.aspx?playerid=" + d.id;
+          let player_url = "https://www.fangraphs.com/statss.aspx?playerid=" + d.id;
           d3.select("#tooltip")
             .append("div")
               .attr("class", "results")
               .append("a")
-              .attr("href",  url)
+              .attr("href",  player_url)
               .attr("target", "_blank")
               .text(d.name + ", " + d.team);
-          console.log(d);
         })
         d3.select("#tooltip")
             .append("div")
@@ -310,59 +306,96 @@ d3.csv(csvfile, function(d, i) {
     }
     else {
       if (selectedPoint != null) {
-        drawPoints(dataset, r);
         // selectedPoint.selected = false;
+        drawPoint(selectedPoint, r);
+        // drawPoints(dataset, r);
+        
       }
     }
-      
-    //console.log(closest);
-    //drawPoints(dataset, r);
   }
-  
-  function drawPoints(set, r) {
+  function setCanvasBackground() {
+    context.save();
     context.fillStyle = "#DBDCDB";
     context.globalAlpha = 0.5;
     context.fillRect(-pad, -pad, fullWidth + pad, fullHeight + pad);
-    context.globalAlpha = 1;
+    context.restore();
+  }
+
+  function drawPoints(set, r) {
+    setCanvasBackground();
+    context.save();
     context.fillStyle = "#313131";
     context.strokeWidth = 1;
 
     set.forEach(d => {
       drawPoint(d, r);
     })
-
+    context.restore();
   }
 
   function drawPoint(d, r) {
+    let overlaps = dataset.filter(e => e.year == d.year && e.fwar == d.fwar).length - 1;
     let cx = x(d.year),
         cy = y(d.fwar);
     
     context.save();
     if (d.selected) {
-      context.fillStyle = "red"
+      context.fillStyle = "#15B094"
+    } else if (d.currentPlayer) {
+      context.fillStyle = "#CE1515";
     } else {
       context.fillStyle = "#313131";
     }
     // Draw Circle
     context.beginPath();
-    context.arc(cx, cy, r, 0, 2 * Math.PI); // Circle
+    context.arc(cx, cy, r + overlaps, 0, 2 * Math.PI); // Circle
     context.closePath();
     context.fill();
     context.stroke();
     context.restore();
+  }
+  console.log(dataset.filter(d => d.PA <= 400))
+  function highlightPlayer(name) {
+    let seasons = dataset.filter(d => name == d.name);
+    let other_seasons = [];
+    for (let i = 0; i < seasons.length; i++) {
+      let others = dataset.filter(d => seasons[i].year == d.year 
+                                    && seasons[i].fwar == d.fwar 
+                                    && seasons[i].name != d.name);
+      for (let j = 0; j < others.length; j++) {
+        others[j].currentPlayer = true;
+        other_seasons.push(others[j])
+      }
+      seasons[i].currentPlayer = true;
+    }
+    console.log(other_seasons)
+    for (let other of other_seasons) {
+      seasons.push(other);
+    }
+    console.log(seasons)
+    drawPoints(seasons, r);
+    if (prev_seasons != null) {
+      for (let j = 0; j < prev_seasons.length; j++) {
+        prev_seasons[j].currentPlayer = false;
+      }
+      console.log(prev_seasons)
+      drawPoints(prev_seasons)
+    }
+    prev_seasons = [];
+    for (let season of seasons) {
+      prev_seasons.push(season);
+    }
+    resetted();
   }
 
   function repeatLastEvent() {
     context.save();
     context.clearRect(0, 0, width, height);
     context.translate(lastEvent.transform.x, lastEvent.transform.y);
-   // console.log(d3.event.transform.x, d3.event.transform.y);
     tk = lastEvent.transform.k;
     context.scale(tk, tk);
-    // console.log(tk);
     r = Math.floor(rScale(tk));
     zoomedR = rScale(tk) * tk;
-    // console.log(tScale(tk));
     drawPoints(dataset, r);
     context.restore();
   }
@@ -373,13 +406,10 @@ d3.csv(csvfile, function(d, i) {
     context.clearRect(0, 0, width, height);
     lastEvent = d3.event;
     context.translate(d3.event.transform.x, d3.event.transform.y);
-   // console.log(d3.event.transform.x, d3.event.transform.y);
     tk = d3.event.transform.k;
     context.scale(tk, tk);
-    // console.log(tk);
     r = Math.floor(rScale(tk));
     zoomedR = rScale(tk) * tk;
-    // console.log(tScale(tk));
     drawPoints(dataset, r);
     context.restore();
     zoomedX = d3.event.transform.rescaleX(x);
@@ -406,27 +436,100 @@ d3.csv(csvfile, function(d, i) {
         .call(zoom.transform, d3.zoomIdentity);
     isZoomed = false;
   }
+  /////  PLAYER SEARCH  /////
+  let searchBox = d3.select("input.search")
+    .attr("placeholder", "Find player")
+    .style("position", "absolute")
+    .style("left", (margin.left + width - 185 - 3) + "px")
+    .style("top", (margin.top - 30) + "px")
+    .style("width", "185px")
+    .style("font-size", "20px")
+    .style("font-family", "Palatino")
+
+  let names = dataset.map(d => d.name);
+  unique_names = names.filter((d, i) => i == names.indexOf(d));
+  console.log(unique_names);
+  d3.select("datalist#names")
+    .selectAll("option")
+    .data(unique_names)
+    .enter()
+    .append("option")
+    .attr("value", d => d);
+
+  searchBox.on("change", function() {
+    let name = d3.select("input.search").property("value");
+    highlightPlayer(name);
+  })
+  // Sources
+  let source = {
+    "label": "data: ",
+    "text": "www.fangraphs.com",
+    "size": 20,
+    "link": "https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=300&type=8&season=2018&month=0&season1=1871&ind=1&team=&rost=&age=&filter=&players=",
+    "font_family": "Calibri",
+    "color": "#21480F"
+  }
+
+  g.append("a")
+      .attr("href", source.link)
+      .attr("target", "_blank")
+      .append("text")
+      .attr("x", -margin.left + 55)
+      .attr("y", height + margin.bottom - 7)
+      .attr("class", "source")
+    .text(source.text)
+      .style("font-size", source.size)
+      .style("text-anchor", "start")
+      .style("font-family", source.font_family)
+      .style("font-style", "italic")
+      .style("fill", source.color)
+      .style("text-shadow", "1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;");
+
+  g.append("text")
+      .attr("x", -margin.left + 5)
+      .attr("y", height + margin.bottom - 7)
+      .attr("class", "source")
+      .text(source.label)
+      .style("font-size", source.size)
+      .style("font-weight", "bold")
+      .style("text-anchor", "start")
+      .style("font-family", source.font_family)
+
+  g.append("a")
+      .attr("href", "https://library.fangraphs.com/war/war-position-players/")
+      .attr("target", "_blank")
+    .append("text")
+      .text("fWAR Explanation")
+      .attr("x", width * 230 / 1000)
+      .attr("y", height + margin.bottom - 7)
+      .style("text-decoration", "underline")
+      .style("font-size", "18px")
+      .style("font-weight", "bold")
+      .style("font-style", "italic")
+      .style("letter-spacing", "0.52px")
+      .style("font-family", "Calibri")
+
 
   let signature = {
     "text": "@SequentialChaos",
     "link": "https://twitter.com/sequentialchaos",
-    "size": 18,
-    "color": "#063C47"
+    "size": 20,
+    "color": "#226978"
   }
 
   svg.append("a")
-    .attr("href", signature.link)
-    .attr("target", "_blank")
+      .attr("href", signature.link)
+      .attr("target", "_blank")
     .append("text")
-    .attr("x", width + margin.left + margin.right - 5)
-    .attr("y", height + margin.top + margin.bottom - 5)
-    .attr("class", "signature")
-    .text(signature.text)
-    .style("fill", signature.color)
-    .style("font-size", signature.size)
-    .style("font-weight", "bold")
-    .style("text-anchor", "end")
-    .style("font-family", "Cambria");
+      .attr("x", width + margin.left + margin.right - 8)
+      .attr("y", height + margin.top + margin.bottom - 8)
+      .attr("class", "signature")
+      .text(signature.text)
+      .style("fill", signature.color)
+      .style("font-size", signature.size)
+      .style("font-weight", "bold")
+      .style("text-anchor", "end")
+      .style("font-family", "Cambria");
 
 })
 
